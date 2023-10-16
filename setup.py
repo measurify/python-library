3b50621fb3b016140da3ed7745dffa75cea7ba38
mattefresta <matteofresta97@gmail.com>
2023-10-16 17:33:04 +0200

    Added setup file

--- setup.py
+++ setup.py
@@ -3,7 +3,8 @@ from setuptools import setup,find_packages
  
 setup(name='measurify',
     version='0.0.1',
-    install_requires=[],
+    install_requires=["numpy","pandas","requests","urllib3"],
     packages=find_packages(),
+    data_files=[('measurify/measurifyConfig.json')],
     py_modules=[]
 )
